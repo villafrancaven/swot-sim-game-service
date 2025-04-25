802041eaf61e4f14b65bbfc1ca28d0353c39c97d
Reyvean <villafrancareyvean@gmail.com>
2025-04-25 17:03:56 +0800

    add monkey patch

--- app/socketio.py
+++ app/socketio.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask_socketio import SocketIO
 import redis
 import os
@@ -6,4 +9,4 @@ redis_url = os.environ.get("REDIS_URL", "redis://localhost:6379")
 
 redis_client = redis.Redis.from_url(redis_url, decode_responses=True)
 
-socketio = SocketIO(cors_allowed_origins="*", message_queue=redis_url)
+socketio = SocketIO(cors_allowed_origins="*", message_queue=redis_url, async_mode="eventlet")
